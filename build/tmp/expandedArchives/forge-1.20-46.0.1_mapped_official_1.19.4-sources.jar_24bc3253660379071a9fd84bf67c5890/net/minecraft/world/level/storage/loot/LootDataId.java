d0fd5b753a36d7a3abb1d9cf18f27a6632ab838e
StyStatic <stystatic@gmail.com>
2023-06-12 22:48:42 -0700

    gitignore

--- build/tmp/expandedArchives/forge-1.20-46.0.1_mapped_official_1.19.4-sources.jar_24bc3253660379071a9fd84bf67c5890/net/minecraft/world/level/storage/loot/LootDataId.java
+++
@@ -1,6 +0,0 @@
-package net.minecraft.world.level.storage.loot;
-
-import net.minecraft.resources.ResourceLocation;
-
-public record LootDataId<T>(LootDataType<T> f_278383_, ResourceLocation f_278500_) {
-}
